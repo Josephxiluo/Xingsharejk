4ed9eaf0dfb74870e21dc25d981ecd7624f1e29d
qizhugong <qizhugong@naeer.com>
2026-05-17 09:58:03 +0800

    删除重构

--- assets/app.js
+++
@@ -1,6 +0,0 @@
-const currentPage = document.body.dataset.page;
-const menuLinks = [...document.querySelectorAll('.menu a')];
-
-menuLinks.forEach((link) => {
-  link.classList.toggle('active', link.dataset.page === currentPage);
-});
